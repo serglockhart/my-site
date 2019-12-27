8de698cbf24c4aa772cc78e7f6496e3f3d58d8d7
lockjio <jio.buenviaje@gmail.com>
2019-12-27 16:08:12 -0500

    Delete Educ card

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,10 +1,8 @@
 import React from "react"
-import Educ from "../Educ/index.js"
 
 const Section = props => (
   <div className="marg-bot-2" id={props.id}>
     <h4 className="title">{props.title}</h4>
-    <Educ educCard={this.props.educCard} />
     {props.button}
   </div>
 )
