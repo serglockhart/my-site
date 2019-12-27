a51219481c7a88737d69a516c8be06a23246337a
lockjio <jio.buenviaje@gmail.com>
2019-12-27 15:40:34 -0500

    Convert technologies to Tech component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Header from "../components/Header/index.js"
 import Section from "../components/Section/index.js"
 import Card from "../components/Card/index.js"
+import Tech from "../components/Tech/index.js"
 import Footer from "../components/Footer/index.js"
 import Hero from "../components/Hero/index.js"
 import Action from "../components/Action/index.js"
@@ -324,34 +325,7 @@ const IndexPage = () => (
         {/* Technologies */}
         <div className="marg-bot-2" id="technologies">
           <h4 className="title">Technologies</h4>
-          <div className="card">
-            <div className="row">
-              <div className="col-12 col-md-6">
-                <h5>General Purpose</h5>
-                <p>JavaScript (ES6), C++, Python, Java, Go/Golang</p>
-                <h5>Web</h5>
-                <p>
-                  React, Redux, Node, Express, jQuery, XML, HTML, CSS
-                  (SCSS/Sass, Bootstrap)
-                </p>
-                <h5>Mobile</h5>
-                <p>DART, Flutter (Provider)</p>
-                <h5>Embedded Systems</h5>
-                <p>Arduino, Raspberry Pi, Intel Galileo</p>
-              </div>
-              <div className="col-12 col-md-6 col-1-format">
-                <h5>Databases &amp; Deployment</h5>
-                <p>
-                  SQL (Postgres, SQL Server), AWS (S3, DynamoDB, Lambda, IoT,
-                  CloudWatch), Firebase, Heroku
-                </p>
-                <h5>Other</h5>
-                <p>MATLAB, Git, Linux, Kali Linux, Agile (SCRUM)</p>
-                <h5>Design</h5>
-                <p>AutoCAD, Fusion 360, SOLIDWORKS, Fritzing, Eagle, Spark</p>
-              </div>
-            </div>
-          </div>
+          <Tech />
           <Link to="#projects" className="btn btn-info">
             View my projects →
           </Link>
