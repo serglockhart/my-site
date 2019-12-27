febfd1adde06180114a2b2d6addc4ab937252d9b
lockjio <jio.buenviaje@gmail.com>
2019-12-27 13:34:07 -0500

    Add button prop

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,6 +5,7 @@ const Section = props => (
   <div className="marg-bot-2" id={props.id}>
     <h4 className="title">{props.title}</h4>
     <Educ educCard={this.props.educCard} />
+    {props.button}
   </div>
 )
 
