67015ab639e7565f8b694f178fc8f5d4ae0a5509
lockjio <jio.buenviaje@gmail.com>
2019-12-27 14:09:14 -0500

    Git config ignore case

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+import scrollTo from "gatsby-plugin-smoothscroll"
+
+const Footer = () => (
+  <footer>
+    <div className="container-fluid">
+      <p className="right">
+        Juan Sergio Buenviaje © 2019. All rights reserved.
+      </p>
+      <p id="scroll-to-top" onClick={() => scrollTo("#top")}>
+        Scroll to top ↑
+      </p>
+    </div>
+  </footer>
+)
+
+export default Footer
---
+++ src/components/Hero.js
@@ -0,0 +1,80 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Background from "../images/home-backing.svg"
+
+const Hero = () => (
+  <div className="marg-bot-1" id="home">
+    <div
+      className="card has-bg"
+      style={{ backgroundImage: `url(${Background})` }}
+      id="hero"
+    >
+      <div className="flex-container no-flex-mobile">
+        <div className="profile-picture-wrapper">
+          <img
+            src={`../../images/me.png`}
+            className={"profile-picture"}
+            alt={"Jio Buenviaje image"}
+            aria-hidden="true"
+          />
+        </div>
+        <div className="flex top">
+          <h1 className="name">Hey! I'm Jio Buenviaje</h1>
+          <p>
+            I aim to leverage tech to better understand and solve pressing
+            problems of niche groups. I study computer science at{" "}
+            <a
+              href="https://cse.nd.edu/undergraduates/computer-science"
+              rel="noopener noreferrer"
+              target="_BLANK"
+            >
+              University of Notre Dame,
+            </a>{" "}
+            and spend my free time{" "}
+            <a
+              href="https://linkedin.com/in/lockjio/"
+              rel="noopener noreferrer"
+              target="_BLANK"
+            >
+              working at startups,
+            </a>{" "}
+            <a
+              href="https://github.com/lockjio"
+              rel="noopener noreferrer"
+              target="_BLANK"
+            >
+              learning new things,
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://instagram.com/lockjio/"
+              rel="noopener noreferrer"
+              target="_BLANK"
+            >
+              simply enjoying life.
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div className="buttons marg-bot-05">
+      <a
+        href={`../../files/resumeBuenviaje.pdf`}
+        rel="noopener noreferrer"
+        target="_BLANK"
+        className="btn btn-info marg-bot-05"
+      >
+        View my resume
+      </a>
+      <Link to="#projects" className="btn btn-info marg-bot-05">
+        View my projects
+      </Link>
+      <a href="mailto:jbuenvia@nd.edu" className="btn btn-info marg-bot-05">
+        Contact me
+      </a>
+    </div>
+  </div>
+)
+
+export default Hero
