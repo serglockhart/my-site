08047f51a88fdca1978d41eb06bdb35378def94d
lockjio <jio.buenviaje@gmail.com>
2019-12-28 00:43:55 -0500

    Fix contact modal

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
 
 const Contact = props => {
-  const { buttonLabel, className } = props
+  const { className } = props
 
   const [modal, setModal] = useState(false)
 
@@ -18,54 +18,52 @@ const Contact = props => {
         <div className="modal-content">
           <ModalHeader toggle={toggle}>Contact Me</ModalHeader>
           <form id="contactForm" method="POST" data-netlify="true">
-            <div className="modal-body">
-              {/* Render a div for error and success messages */}
+            <div class="modal-body">
               <div id="message"></div>
-              {/* Render the form fields */}
-              <div className="row">
-                <div className="col-12 col-md-6">
+              <div class="row">
+                <div class="col-12 col-md-6">
                   <input
                     type="text"
                     name="firstName"
                     id="firstName"
-                    className="form-control"
+                    class="form-control"
                     placeholder="First"
                     required="true"
-                  ></input>
+                  />
                 </div>
-                <div className="col-12 col-md-6">
+                <div class="col-12 col-md-6">
                   <input
                     type="text"
                     name="lastName"
-                    className="form-control"
+                    class="form-control"
                     placeholder="Last"
                     required="true"
-                  ></input>
+                  />
                 </div>
               </div>
               <input
                 type="email"
                 name="email"
-                className="form-control"
+                class="form-control"
                 placeholder="example@gmail.com"
                 required="true"
-              ></input>
+              />
               <input
                 type="text"
                 name="subject"
-                className="form-control"
+                class="form-control"
                 placeholder="Subject..."
                 required="true"
-              ></input>
+              />
               <textarea
                 type="text"
                 name="body"
-                className="form-control"
+                class="form-control"
                 placeholder="Message..."
                 required="true"
               ></textarea>
             </div>
-            <div className="modal-footer">
+            <div class="modal-footer">
               <input
                 type="submit"
                 class="btn btn-info"
@@ -74,8 +72,8 @@ const Contact = props => {
               />
               <button
                 type="button"
-                onClick={toggle}
-                className="btn btn-secondary"
+                class="btn btn-secondary"
+                data-dismiss="modal"
               >
                 Cancel
               </button>
