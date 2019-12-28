a0d6870c53a7c8cb4273136fc4499c4937f286b0
lockjio <jio.buenviaje@gmail.com>
2019-12-28 15:46:48 -0500

    Update meta data

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,33 @@ const IndexPage = props => {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <meta name="author" content="Jio Buenviaje" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Jio Buenviaje" />
+        <meta property="og:url" content="jbuenviaje.com" />
+        <meta property="twitter:url" content="jbuenviaje.com" />
+        <meta name="theme-color" content="#ffffff" />
         <title>Jio Buenviaje</title>
+        <meta property="og:title" content="Jio Buenviaje" />
+        <meta property="twitter:title" content="Jio Buenviaje" />
+        <meta
+          name="description"
+          content="Personal website of Juan Sergio Buenviaje: lifelong learner, driver developer, and proud Filipino."
+        />
+        <meta
+          property="og:description"
+          content="Personal website of Juan Sergio Buenviaje: lifelong learner, driver developer, and proud Filipino."
+        />
+        <meta
+          property="twitter:description"
+          content="Personal website of Juan Sergio Buenviaje: lifelong learner, driver developer, and proud Filipino."
+        />
+        <meta
+          name="keywords"
+          content="juan,sergio,jio,juansergio,juansergiobuenviaje,jiobuenviaje,notredame,nd,freelance,web,webdev,mobile,iosdevelopment,ios,android,androiddevelopment,website,cs,filipino,southbend,indiana,university,philippines,philippine,student,react,javascript,node,developer,dev,flutter,dart,embedded,arduino,contact,new,york,city,NY,youthhack,cbs"
+        />
+        <meta property="og:image" content="../../images/bg.png" />
+        <meta property="twitter:image" content="../../images/bg.png" />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="canonical" href="https://jbuenviaje.com/" />
       </Helmet>
       {/* Navigation */}
