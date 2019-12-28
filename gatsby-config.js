637b6c4fad512a17a883ee01e6a6a35a442b12a3
lockjio <jio.buenviaje@gmail.com>
2019-12-28 12:18:23 -0500

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jio Buenviaje`,
+    description: `Personal website of Juan Sergio Buenviaje: lifelong learner, driver developer, and proud Filipino.`,
+    author: `@lockjio`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
