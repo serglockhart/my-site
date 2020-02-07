dc115247c6c61bd11f9e7becea0bdb0271841b36
lockjio <jio.buenviaje@gmail.com>
2020-02-07 11:25:24 -0500

    Automate copyright year

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,8 @@ const Footer = props => {
     <footer>
       <div className="container-fluid">
         <p className="right">
-          Juan Sergio Buenviaje © 2020. All rights reserved.
+          Juan Sergio Buenviaje © {new Date().getFullYear()}. All rights
+          reserved.
         </p>
         <p id="scroll-to-top" onClick={() => scrollTo("#top")}>
           Scroll to top ↑
