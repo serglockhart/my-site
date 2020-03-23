567d7d94beab6270daf581953ced20da5e372fa2
lockjio <jio.buenviaje@gmail.com>
2020-03-23 12:21:41 -0400

    Update tech

--- src/components/Tech/Tech.js
+++ src/components/Tech/Tech.js
@@ -42,8 +42,8 @@ const Tech = props => {
             </p>
             <h5>Design</h5>
             <p>
-              Affinity Designer, Sketch, Figma, Adobe XD, Zeplin, AutoCAD,
-              Fusion 360, SOLIDWORKS, Fritzing, EAGLE
+              Affinity Designer, Sketch, Figma, Adobe XD, Invision Studio,
+              Zeplin, AutoCAD, Fusion 360, SOLIDWORKS, Fritzing, EAGLE
             </p>
           </div>
         </div>
