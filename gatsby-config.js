c2b5206a53f052a9e53cc741c9540998e62ad5c0
lockjio <jio.buenviaje@gmail.com>
2020-03-31 22:41:09 -0400

    Setup styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: "gatsby-plugin-html-attributes",
       options: {
         lang: "en",
