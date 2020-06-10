46ce3d773e7f76827bf0cf87ca90b63644fd0f8e
lockjio <jio.buenviaje@gmail.com>
2020-06-10 18:25:46 -0400

    Fix favicon path

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/favicon.png`, // This path is relative to the root of the site.
+        icon: `./src/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typescript`,
