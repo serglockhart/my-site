869f1196f162ee507535f0d312ebf0e3260fe6d5
lockjio <jio.buenviaje@gmail.com>
2020-06-24 02:36:56 -0400

    Finish Browser component gatsby-image implement

--- src/components/Browser.tsx
+++ src/components/Browser.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Img from "gatsby-image"
 
 const Browser = (props: {
   image: string
@@ -43,8 +44,8 @@ const Browser = (props: {
         </div>
         <picture className="img-fluid">
           <source srcSet={props.image} type="image/webp" />
-          <img
-            src={props.imageBackup}
+          <Img
+            fluid={props.imageBackup}
             alt={props.imageAlt}
             aria-label={props.imageAlt}
             className="img-fluid"
