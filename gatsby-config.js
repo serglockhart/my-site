06c989d496ebe066f06936cd07e45515d8e73b68
lockjio <jio.buenviaje@gmail.com>
2020-06-24 02:35:55 -0400

    Setup gatsby-image folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
