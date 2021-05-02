75b77cddd0179fe7cb98fe7ae76ec929dd7c9483
lockjio <jio.buenviaje@gmail.com>
2021-05-02 01:50:55 -0400

    Simplify tech stack

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -8,47 +8,42 @@ const Tech = () => {
           <div className="col-12 col-md-6">
             <h5>General Purpose</h5>
             <p>
-              JavaScript ES6, TypeScript, Python, Go/Golang, Dart, Java, C/C++
+              JavaScript, TypeScript, Python, Go/Golang, Dart, Java, C/C++
               MATLAB
             </p>
             <h5>Web</h5>
             <p>
-              React (Gatsby, Next), Redux (Saga, Thunk), Node, Express, Flask,
-              Django, GraphQL, Apollo, Hugo, Liquid, jQuery, HTML, CSS
-              (Sass/SCSS, Bootstrap, Material-UI, styled-components, Tailwind,
-              Bulma, LESS), npm, Yarn
+              React, Redux, Thunk, Gatsby, Next, Node, Express, Django, Flask,
+              GraphQL, Apollo, Grafana, EJS, Liquid, jQuery, HTML/CSS
             </p>
             <h5>Mobile</h5>
             <p>
-              iOS, Swift, UIKit, SwiftUI, Swift Package Manager, CocoaPods, Core
-              Data, Realm, Alamofire, TestFlight, Lottie, Xcode, Android,
-              Gradle, Play Console, Android Studio, Flutter, Provider, Wiredash,
-              Ionic, Capacitor, Expo
+              Swift, SwiftUI, CocoaPods, Alamofire, TestFlight, Dart, Flutter,
+              Capacitor, Lottie
             </p>
-            <h5>Embedded</h5>
-            <p>Raspberry Pi, Arduino, Verilog</p>
+            <h5>Styling</h5>
+            <p>
+              SCSS/SASS, LESS, Tailwind, Bootstrap, Bulma, Material-UI, Ant
+              Design, styled-components
+            </p>
           </div>
           <div className="col-12 col-md-6 col-1-format">
             <h5>Databases &amp; Deployment</h5>
             <p>
-              PostgreSQL, Microsoft SQL Server, SQL, MongoDB, InfluxDB, Parse
-              Platform, Amazon Web Services (AWS), Firebase, Heroku, Netlify,
-              Vercel (ZEIT Now), Docker, Akamai, Fastly
+              PostgreSQL, SQL, Firebase, Amazon Web Services (AWS), MongoDB,
+              Mongoose, InfluxDB, Heroku, Netlify, Vercel
             </p>
             <h5>Testing</h5>
-            <p>Jest, Enzyme, Cypress, Postman</p>
+            <p>Jest, Enzyme, Postman</p>
             <h5>Other</h5>
             <p>
-              Git (GitHub, GitLab), UNIX/Linux, Bash (Shell), Progressive Web
-              Apps (PWA), JSON, XML, YAML, DOM, REST APIs, HTTPS, DNS, CDN,
-              Agile Software Development, Scrum, Kanban, Jira, Grafana
+              Git, Linux, Bash (Shell), Progressive Web Apps (PWA), JSON, XML,
+              REST APIs, Agile Software Development, Scrum, Kanban, Jira
             </p>
             <h5>Design</h5>
             <p>
-              Sketch, Figma, Adobe XD, Illustrator, Photoshop, InDesign,
-              Affinity Designer, InVision Studio, Supernova Studio, Zeplin,
-              Framer, Webflow, Squarespace, Wix, Canva, AutoCAD, Fusion 360,
-              SolidWorks
+              Adobe Illustrator, Adobe Photoshop, Adobe InDesign, Adobe XD,
+              Sketch, Figma, Zeplin, Supernova Studio, Webflow, Squarespace
             </p>
           </div>
         </div>
