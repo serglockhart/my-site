4179638bc45846aae454f326420daaabcf89f3a4
lockjio <jio.buenviaje@gmail.com>
2021-10-19 19:58:32 -0400

    Switch imports from css to gatsby-browser

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/600.css"
+import "@fontsource/open-sans/700.css"
+import "@fontsource/open-sans/800.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "@popperjs/core/dist/umd/popper.min.js"
